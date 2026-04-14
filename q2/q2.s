f8f06f9063978d6d6a9158d3296215dea6df86b9
vaishnavi <nelantivaishnavi@gmail.com>
2026-04-14 16:11:32 +0530

    changed

--- q2/q2.s
+++ q2/q2.s
@@ -21,15 +21,15 @@ main:
 
   
     slli a0, s0, 2    # a0 = n * 4 bytes
-    call malloc       # Allocate space for 'arr'
+    call malloc       # allocate space for arr
     mv s4, a0         # s4 stores base address of arr
 
     slli a0, s0, 2    # a0 = n * 4 bytes
-    call malloc       # Allocate space for 'ans'
+    call malloc       # allocate space for ans
     mv s5, a0         # s5 stores base address of ans
 
     slli a0, s0, 2    # a0 = n * 4 bytes
-    call malloc       # Allocate space for 'stack'
+    call malloc       # allocate space for stack
     mv s6, a0         # s6 stores base address of stack
    
 
@@ -51,14 +51,14 @@ convert_loop:
     j convert_loop
 
 nge:
-    addi s2,s0,-1       # i = n - 1 (Right to Left)
+    addi s2,s0,-1       # i = n - 1 
     li s3,-1            #top of stack = -1
 
     li t0,0             #i
 
 initialize:
     bge t0,s0,loop         #if i>=n exit curr loop
-    mv t1, s5               #t1 stores adress of dynamic ans (malloced s5)
+    mv t1, s5               #t1 stores adress of ans
     slli t2,t0,2            # offset calc t2 = i*4
     add t1,t1,t2            #t1=adress+offset
     li t3, -1               
@@ -72,19 +72,19 @@ loop:
 while:
 
     blt s3, zero, assign       #if stack.top()==0 push element into stack
-    mv t0, s6               # t0 stores address of dynamic stack (malloced s6)
+    mv t0, s6              
     slli t1,s3,2            #offset calc t1 = stack.top()*4
     add t0,t0,t1            #adress calc t0 = base add + offset
     lw t2,0(t0)             #t2 now contains value of stack top
 
-    mv t3, s4               # use malloced arr (s4)
+    mv t3, s4              
     slli t4, s2, 2
     add t3, t3, t4
     lw t5, 0(t3)        #t5 contains value of arr[i]
 
     #now we want arr[stack.top()]
 
-    mv t6, s4               # use malloced arr (s4)
+    mv t6, s4               
     slli t1, t2, 2
     add t6, t6, t1
     lw t6, 0(t6)       #t7 contains arr[stack.top()]
@@ -95,13 +95,13 @@ while:
 
 
 assign:
-    mv t3, s5               # use malloced ans (s5)
+    mv t3, s5              
     slli t4, s2, 2
     add t3, t3, t4
     blt s3, zero, no_elem
 
     # result[i] = stack[top]
-    mv t0, s6               #  use malloced stack (s6)
+    mv t0, s6             
     slli t1, s3, 2
     add t0, t0, t1
     lw t2, 0(t0)
@@ -115,7 +115,7 @@ no_elem:
   
 push:
     addi s3, s3, 1      #incrementing tos
-    mv t0, s6           #  use malloced stack (s6)
+    mv t0, s6           
     slli t1, s3, 2
     add t0, t0, t1
     sw s2, 0(t0)        #storing i on top of stack
@@ -129,7 +129,7 @@ done:
     addi s0,s0,-1
 print:
     bge s2, s0, print_last
-    mv t0, s5           # FIXED: use malloced ans (s5)
+    mv t0, s5          
     slli t1, s2, 2
     add t0, t0, t1
     lw a1, 0(t0)
@@ -140,14 +140,14 @@ print:
 
 print_last:
 
-    mv t0, s5           # use malloced ans (s5)
+    mv t0, s5           
     slli t1, s2, 2
     add t0, t0, t1
     lw a1, 0(t0)
     la a0, fmt2
     call printf
 
-done_print:             # Label for early exit if no args
+done_print:            
 
 end:
     # Restoring all registers from stack
