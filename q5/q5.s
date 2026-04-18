8f2c4d05baefafac06c3cd161679b6070160da01
vaishnavi <nelantivaishnavi@gmail.com>
2026-04-18 17:08:38 +0530

    Update q5.s

--- q5/q5.s
+++ q5/q5.s
@@ -1,14 +1,10 @@
 .section .data
 filename: .string "input.txt"
 mode:     .string "r"
-fmt2: .string "%s"
+fmt2:     .string "%s"
 
-buffer :
-.space 10004
-yes:
-.string "Yes\n"
-no:
-.string "No\n"
+yes: .string "Yes\n"
+no:  .string "No\n"
 
 .section .text
 .globl main
@@ -18,55 +14,27 @@ main:
     sd ra, 24(sp)
 
     # fopen(filename, "r")
-    la a0, filename    #first argument
-    la a1, mode        #second argument
+    la a0, filename
+    la a1, mode
     call fopen
-    mv s0, a0          # file pointer stored in s0
+    mv s0, a0
 
-    beqz s0, finish
+    beqz s0, print_no   # file open failed
 
+    # go to end
     mv a0, s0
-    la a1, fmt2
-    la a2, buffer
-    
-    mv a0,s0
-    li a1,0     #zero to move forward
-    li a2,2     #to move to end of file
+    li a1, 0
+    li a2, 2
     call fseek
 
-    mv a0,s0
+    # get size
+    mv a0, s0
     call ftell
-    mv s1,a0        #move to end of file to get size
+    mv s1, a0
 
-    addi t1,s1,-1
+    addi t1, s1, -1   # last index
 
-    mv a0, s0
-    mv a1,t1
-    li a2,0
-    call fseek
-
-    mv a0,s0
-    call fgetc
-    li t4, 10
-    bne a0, t4, skip_nl
-
-    addi t1, t1, -1
-
-skip_nl:
-    li t2, 0
-
-check:
-
-    bge t2, t1, end1
-    mv a0, s0
-    mv a1, t2
-    li a2, 0
-    call fseek
-
-    mv a0, s0
-    call fgetc
-    mv t3, a0 
-
+    # check if last char is newline
     mv a0, s0
     mv a1, t1
     li a2, 0
@@ -74,30 +42,58 @@ check:
 
     mv a0, s0
     call fgetc
-    mv t4, a0 
+    li t4, 10
+    bne a0, t4, skip_nl
 
-    bne t3, t4, finish
+    addi t1, t1, -1   # ignore newline
+
+skip_nl:
+    li t2, 0          # start index
+
+check:
+    bge t2, t1, print_yes
+
+    # get char at start (t2)
+    mv a0, s0
+    mv a1, t2
+    li a2, 0
+    call fseek
+
+    mv a0, s0
+    call fgetc
+    mv t3, a0
+
+    # get char at end (t1)
+    mv a0, s0
+    mv a1, t1
+    li a2, 0
+    call fseek
+
+    mv a0, s0
+    call fgetc
+    mv t4, a0
+
+    bne t3, t4, print_no
 
     addi t2, t2, 1
     addi t1, t1, -1
     j check
 
-end1:
+print_yes:
+    la a0, fmt2
+    la a1, yes
+    call printf
+    j cleanup
 
-la a0,fmt2
-la a1,yes
-call printf
-ld ra, 24(sp)
-addi sp, sp, 32
-ret
+print_no:
+    la a0, fmt2
+    la a1, no
+    call printf
 
+cleanup:
+    mv a0, s0
+    call fclose
 
-finish:
-
-la a0,fmt2
-la a1,no
-call printf
-ld ra, 24(sp)
-addi sp, sp, 32
-ret
-
+    ld ra, 24(sp)
+    addi sp, sp, 32
+    ret
